ee08fb6b7a0582da7040ad98cfef1f634063f8f9
andy <andy@ashall.co>
2017-10-10 11:33:06 +0100

    added catch error

--- index.py
+++ index.py
@@ -1,10 +1,12 @@
 import requests
-import json
 from jsontraverse.parser import JsonTraverseParser
 
 r = requests.get("https://www.reddit.com/r/python.json")
 
 parser = JsonTraverseParser(r.text)
 
-for x in range(1,25):
-  print(f"{x}. " + parser.traverse(f"data.children.{x-1}.data.title"))
+if parser.traverse("error") is None:
+  for x in range(1,25):
+    print(f"{x}. " + parser.traverse(f"data.children.{x-1}.data.title"))
+else:
+  print("Error")
